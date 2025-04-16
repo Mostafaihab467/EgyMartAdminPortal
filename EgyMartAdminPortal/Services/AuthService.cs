f3530f41c0d386c5d0464fd4bfa0bff6ce9195f4
Mina Samir <minasamir9749@gmail.com>
2025-04-16 17:06:10 +0200

    last-update-16-04-2025

--- EgyMartAdminPortal/Services/AuthService.cs
+++ EgyMartAdminPortal/Services/AuthService.cs
@@ -3,7 +3,6 @@ using Microsoft.AspNetCore.Components;
 using Microsoft.JSInterop;
 using System.Net.Http.Json;
 using System.Text.Json;
-using static System.Net.WebRequestMethods;
 
 namespace EgyMartAdminPortal.Services
 {
